f75f39ca3cc490a211e1cbe098064f992d27fef9
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-07-29 16:33:19 +0700

    v1.0.8

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME="rml-sales-visit-v1-0-7-direct-pdf";
+const CACHE_NAME="rml-sales-visit-v1-0-8-logout-navigation-fix";
 const APP_SHELL=[
  "./",
  "./index.html",
  "./style.css","./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-0-7.js",
+ "./app-v1-0-8.js",
  "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
