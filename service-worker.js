3e3843a41e3ced0eb9669090871f5ba8effdeeaf
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-07-29 11:32:05 +0700

    Secure Login

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,14 @@
-
-const CACHE_NAME="rml-sales-visit-v0-10-7-admin-name";
+const CACHE_NAME="rml-sales-visit-v0-10-8-secure-login";
 const APP_SHELL=[
  "./",
  "./index.html",
  "./style.css",
- "./app.js",
- "./customers.js",
+ "./app-v0-10-8.js",
+ "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
  "./icon-512.svg"
 ];
-
-self.addEventListener("install",event=>{
- event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
- self.skipWaiting();
-});
-
-self.addEventListener("activate",event=>{
- event.waitUntil(
-   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
- );
- self.clients.claim();
-});
-
-self.addEventListener("fetch",event=>{
- if(event.request.method!=="GET")return;
- event.respondWith(
-   caches.match(event.request).then(cached=>{
-     if(cached)return cached;
-     return fetch(event.request).then(response=>{
-       const copy=response.clone();
-       caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
-       return response;
-     }).catch(()=>caches.match("./index.html"));
-   })
- );
-});
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting();});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html"))));});
