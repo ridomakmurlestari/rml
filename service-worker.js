550cb2eb3b02c45b42e4f70e393ca3dcea72b53f
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-07-29 15:47:47 +0700

    Update Source

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME="rml-sales-visit-v1-0-3-fast";
+const CACHE_NAME="rml-sales-visit-v1-0-4-auto-sync";
 const APP_SHELL=[
  "./",
  "./index.html",
  "./style.css",
- "./app-v1-0-3.js",
+ "./app-v1-0-4.js",
  "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
