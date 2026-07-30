e2a8c31f21b4f501bff915d02f87346311a0339a
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-07-30 16:05:19 +0700

    Order Auto Input

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME="rml-sales-visit-v1-2-0-stage1-order-payment";
+const CACHE_NAME="rml-sales-visit-v1-2-0-auto-order-input";
 const APP_SHELL=[
  "./",
  "./index.html",
  "./style.css","./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-2-0-stage1.js",
+ "./app-v1-2-0-auto-order.js",
  "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
