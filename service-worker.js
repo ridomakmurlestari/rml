bc0357bd2ee5905a80e971580298f30e4a615803
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-07-31 09:19:07 +0700

    Sales Riwayat Hari ini

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME="rml-sales-visit-v1-2-4-payment-last-pending";
+const CACHE_NAME="rml-sales-visit-v1-2-5-sales-history-today";
 const APP_SHELL=[
  "./",
  "./index.html",
- "./style-v1-2-4.css",
+ "./style-v1-2-5.css",
  "./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-2-4.js",
+ "./app-v1-2-5.js",
  "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
