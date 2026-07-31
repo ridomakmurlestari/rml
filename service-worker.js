4f65b69cc2c37c99adaa29994ab7f5bc19a373ac
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-07-31 09:12:13 +0700

    Sales Visit v1.2.4

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME="rml-sales-visit-v1-2-3-customer-active-sync";
+const CACHE_NAME="rml-sales-visit-v1-2-4-payment-last-pending";
 const APP_SHELL=[
  "./",
  "./index.html",
- "./style-v1-2-3.css",
+ "./style-v1-2-4.css",
  "./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-2-3.js",
+ "./app-v1-2-4.js",
  "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
