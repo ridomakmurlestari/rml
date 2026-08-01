517a85f8e95c23c1508d8269bca8eff95d94f9db
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-08-01 14:31:18 +0700

    Product Save Fix v1.4,3

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,9 @@
-const CACHE_NAME="rml-sales-visit-v1-4-2-all-products";
+const CACHE_NAME="rml-sales-visit-v1-4-3-product-save-fix";
 const APP_SHELL=[
- "./",
- "./index.html",
- "./style-v1-4-2.css",
+ "./","./index.html","./style-v1-4-2.css",
  "./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-4-2.js",
- "./customers-v0-10-3.js",
- "./manifest.webmanifest",
- "./icon-192.svg",
- "./icon-512.svg"
+ "./app-v1-4-3.js","./customers-v0-10-3.js",
+ "./manifest.webmanifest","./icon-192.svg","./icon-512.svg"
 ];
 self.addEventListener("install",event=>{
  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)));
@@ -24,13 +19,8 @@ self.addEventListener("fetch",event=>{
  event.respondWith((async()=>{
   try{
    const response=await fetch(request,{cache:"no-store"});
-   if(response&&response.ok){
-    const cache=await caches.open(CACHE_NAME);
-    cache.put(request,response.clone());
-   }
+   if(response&&response.ok){const cache=await caches.open(CACHE_NAME);cache.put(request,response.clone())}
    return response;
-  }catch(_){
-   return (await caches.match(request)) || (request.mode==="navigate" ? await caches.match("./index.html") : Response.error());
-  }
+  }catch(_){return (await caches.match(request))||(request.mode==="navigate"?await caches.match("./index.html"):Response.error())}
  })());
 });
