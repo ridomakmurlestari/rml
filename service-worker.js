22ecbb6c2ad6d774521c5d73787a493eef624020
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-08-01 10:42:13 +0700

    Edit Product

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME="rml-sales-visit-v1-3-2-products";
+const CACHE_NAME="rml-sales-visit-v1-3-3-product-modal";
 const APP_SHELL=[
  "./",
  "./index.html",
- "./style-v1-3-2.css",
+ "./style-v1-3-3.css",
  "./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-3-2.js",
+ "./app-v1-3-3.js",
  "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
