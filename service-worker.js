1fd62ba576cffba7824c5b2ec88a825a8d8beafc
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-08-01 15:41:30 +0700

    v1.4.4

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME="rml-sales-visit-v1-4-3-product-save-fix";
 const APP_SHELL=[
  "./","./index.html","./style-v1-4-2.css",
  "./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-4-3.js","./customers-v0-10-3.js",
+ "./app-v1-4-4.js","./customers-v0-10-3.js",
  "./manifest.webmanifest","./icon-192.svg","./icon-512.svg"
 ];
 self.addEventListener("install",event=>{
