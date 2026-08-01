095bfce0ca29a667e0ae373a759011b09bc66171
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-08-01 09:39:33 +0700

    Auto Draft

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME="rml-sales-visit-v1-2-5-sales-history-today";
+const CACHE_NAME="rml-sales-visit-v1-2-6-auto-draft";
 const APP_SHELL=[
  "./",
  "./index.html",
  "./style-v1-2-5.css",
  "./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-2-5.js",
+ "./app-v1-2-6.js",
  "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
