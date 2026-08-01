7c5a7b6aedcff653c13eb65de28513e91397fb30
ridomakmurlestari <ridomakmurlestari@gmail.com>
2026-08-01 11:50:45 +0700

    Import Update Admin V1.3.7

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME="rml-sales-visit-v1-3-6-product-shop-assignment";
+const CACHE_NAME="rml-sales-visit-v1-3-7-import-update-price";
 const APP_SHELL=[
  "./",
  "./index.html",
- "./style-v1-3-6.css",
+ "./style-v1-3-7.css",
  "./pdf-preview.html","./pdf-preview.css","./pdf-preview.js",
- "./app-v1-3-6.js",
+ "./app-v1-3-7.js",
  "./customers-v0-10-3.js",
  "./manifest.webmanifest",
  "./icon-192.svg",
